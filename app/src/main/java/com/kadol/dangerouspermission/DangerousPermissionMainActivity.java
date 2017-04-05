5cdfb76fb574f54fe273d20d36d330b8fb20f7d3
shahednissan <shahednissan@gmail.com>
2017-04-05 20:41:43 +0600

    Initial commit

---
+++ app/src/main/java/com/kadol/dangerouspermission/DangerousPermissionMainActivity.java
@@ -0,0 +1,13 @@
+package com.kadol.dangerouspermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DangerousPermissionMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangerous_permission_main);
+    }
+}
